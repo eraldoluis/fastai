2964e6f19575acbb1516632762a0e04876b33c9d
Eraldo R. Fernandes <eraldo@facom.ufms.br>
2018-09-27 16:27:54 -0400

    Moved to torch.sigmoid (instead of F.sigmoid)

--- fastai/column_data.py
+++ fastai/column_data.py
@@ -131,7 +131,7 @@ class MixedInputModel(nn.Module):
             else:
                 x = F.log_softmax(x)
         elif self.y_range:
-            x = F.sigmoid(x)
+            x = torch.sigmoid(x)
             x = x*(self.y_range[1] - self.y_range[0])
             x = x+self.y_range[0]
         return x
